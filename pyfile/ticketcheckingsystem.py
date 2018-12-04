500ae1b38ae98784caf5063fec14754b1b450bf7
ClareTT <wanglin_35@163.com>
2018-12-04 10:09:37 +0800

    Update

--- pyfile/ticketcheckingsystem.py
+++ pyfile/ticketcheckingsystem.py
@@ -9,100 +9,90 @@ Update on Tuesday Nov 27 18:01:39 2018
 import requests
 import json
 import pandas as pd
-import datetime
 import numpy as np
 from functools import reduce
 
-    
+from functions import traindate
+from functions import fromstationname
+from functions import tostationname
+from functions import isfast
+from functions import text1
+from functions import text2
+from functions import gain_userInteraction
+from functions import isbegin_end
+
+# https://www.cnblogs.com/yanfengt/p/6305542.html 全局变量的引用和修改
+stationNameMapping = pd.read_csv('themappingfile.csv')
+stationNameMapping.columns = ['charactor', 'alpha']
+stationNameMapping.index = stationNameMapping.charactor
+
+
 class ticketcheckingsystem(object):
     "define a class of ticket checking system."
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.results = self.getrawinformation()
+        self.informationTotal = self.gettraininformation()
+        self.parameter = self.generate_filteruserselect()
+        self.condition = self.generate_filterboolcolumn()
         
-    def gain_nowtime(self):
-        "获取当前日期"
-        return datetime.datetime.now().strftime('%Y-%m-%d')
         
-    def fromStationName(self):
-        return input("Where shall you START your travel(eg: '北京'): ")
+        
+    def printinfo(self):
+        print('\nInformation has been collected successfully!')
+        print("\n1. 出发时间\t\t2. 到达时间\t\t3. 高速/普通\n\
+4. 出发站\t\t5. 到达站\n\nFiltering...")
     
-    def toStationName(self):
-        return input("And the Destination(eg: '上海'): ")
-    
-    def traindate(self):
-        dateNow = self.gain_nowtime()
-        yearNow = dateNow[0:5]# 2018-
-        return yearNow + input('When to go(FORMAT: ' + dateNow + '): ' + yearNow)
-    
-    def text1(self):
-        return "\n00:00~06:00(1)\t06:00~09:00(2)\n09:00~12:00(3)\
-\t12:00~15:00(4)\n15:00~18:00(5)\t18:00~21:00(6)\n21:00~23:59(7)\n\
-Tell me when would you like to START most(SECRET: \nmost people \
-choose to type '34', you can also type numbers separated by SPACE \
-like '3 4'), type your number: "
-                                          
-    def text2(self):
-        return "G/D\t\t\tT/Z/.etc\n\
-Select one('1' for the left one and '2' for the right one, OR ignore): "
-    
-    def gotmap(self, cols, informationTotal):
-        "得到该次搜索出发/到达车站名字列表"
-        beginList = []
-        for x in informationTotal[cols]:
-            if (x not in beginList):
-                beginList = beginList + [x]
-        return beginList
-    
-    def isbegin_end(self, dataframe, cols, val):
-        "判断是出发还是到达"
-        a = dataframe[dataframe[cols] == val].index.tolist()
-        return [True, False][a == []]
+    def getrawinformation(self):
+        """
+        The first STEP, get train information:
+            1. get the query URL
+            2. crawl information
+        """
+        
+        url = self.getqueryurl()
+        r = requests.get(url)
+        results = json.loads(r.content)
+        return results
+        
 
-    def isfast(self, dataframe, trainNum):
-        "根据车次首字母判断是否为快速列车，返回bool列表"
-        newList = []
-        for x in dataframe[trainNum]:
-            x = x.lower()
-            newList.append([False, True][('c' in x) | ('g' in x) | ('d' in x)])
-        return newList
-
-    def gain_userInteraction(self, s, defaultPara):
-        while True:
-            para1 = input(s)
-            if para1 == '':
-                para1 = defaultPara
-            para1 = ''.join(para1.split())# 空格统统去掉
-            if para1.isdecimal():# isalnum()意思是判断是否为数字或字母 BUG/输错了，比如超出阈值
-                break
-            print('Please type a numberic thing!!')
-            print('One more chance: ')
-        return para1
     
-    def gain_urlchecking(self, station, from_station_name, to_station_name, train_date):
+    def getqueryurl(self):
         """
         获取查询存放数据url
         
         eg：from_station_name = '北京'，to_station_name = '上海'
         train_date格式为2018-10-05
+                
+        To get the query URL:
+            1. we get user interaction information
+            2. generate query URL and return
         """
-        
 
-        station.columns = ['charactor', 'alpha']
-        station.index = station.charactor
-        from_station = station.loc[from_station_name, 'alpha']
-        to_station = station.loc[to_station_name, 'alpha']
-        return "https://kyfw.12306.cn/otn/leftTicket/query?\
+        from_station = stationNameMapping.loc[fromstationname(), 'alpha']
+        to_station = stationNameMapping.loc[tostationname(), 'alpha']
+        queryUrl = "https://kyfw.12306.cn/otn/leftTicket/query?\
 leftTicketDTO.train_date={}&leftTicketDTO.from_station={}\
 &leftTicketDTO.to_station={}&purpose_codes=ADULT\
-".format (train_date, from_station, to_station)# 该URL通过browser抓包得到，会变化，及时更新
+".format (traindate(), from_station, to_station)# 该URL通过browser抓包得到，会变化，及时更新
+        return queryUrl
 
         # 2018年10月11日'...queryA?...'>>>'...queryO?...'
         # 2018年11月27日'...queryO?...'>>>'...query?...'
 
     
-    def gain_information(self, results):
-        "获取车次信息等"
+    def gettraininformation(self):
+        """
+        获取车次信息等
         
+        To get train information:
+            1. we extract useful information from raw information
+            2. set index and columns
+            3. add new column of mapped station name(in Chinese)
+            4. add new column of if the station is the very begining or end
+            4. add new column of if the train is the fast or an ordinary one
+        
+        """
+        results = self.results
         informationTotal = pd.DataFrame()
         for i in results['data']['result']:
             infoEach = i.split('|')
@@ -158,46 +148,32 @@ leftTicketDTO.train_date={}&leftTicketDTO.from_station={}\
         informationTotal['isVeryEnd'] = np.where(informationTotal['endCity'] 
                        == informationTotal['endName'], True, False)
         
-        informationTotal['isFast'] = self.isfast(informationTotal, 'TrainNum')
+        informationTotal['isFast'] = isfast(informationTotal, 'TrainNum')
         informationTotal['isSlow'] = [not(x) for x in informationTotal.isFast]
         
-        return (mapData, informationTotal)
+        return informationTotal
     
     
-    def gain_conditionTime(self, beginEnd, informationTotal):
-        conditionTime1 = (informationTotal[beginEnd] >= '00:00') & \
-        (informationTotal[beginEnd] <= '06:00')# 筛选条件1：时间段00：00~06：00
-        conditionTime2 = (informationTotal[beginEnd] > '06:00') & \
-        (informationTotal[beginEnd] <= '09:00')# 和筛选条件1互斥
-        conditionTime3 = (informationTotal[beginEnd] > '09:00') & \
-        (informationTotal[beginEnd] <= '12:00')# 和筛选条件1互斥
-        conditionTime4 = (informationTotal[beginEnd] > '12:00') & \
-        (informationTotal[beginEnd] <= '15:00')# 和筛选条件1互斥
-        conditionTime5 = (informationTotal[beginEnd] > '15:00') & \
-        (informationTotal[beginEnd] <= '18:00')# 和筛选条件1互斥
-        conditionTime6 = (informationTotal[beginEnd] > '18:00') & \
-        (informationTotal[beginEnd] <= '21:00')# 和筛选条件1互斥
-        conditionTime7 = (informationTotal[beginEnd] > '21:00') & \
-        (informationTotal[beginEnd] <= '23:59')# 和筛选条件1互斥
-        
-        columnTime = ['con1', 'con2', 'con3', 'con4', 'con5', 'con6', 'con7']
-        conditionTime = pd.DataFrame(list(zip(conditionTime1, 
-                                                   conditionTime2, 
-                                                   conditionTime3, 
-                                                   conditionTime4, 
-                                                   conditionTime5, 
-                                                   conditionTime6, 
-                                                   conditionTime7)))
-        conditionTime.columns = columnTime
-        
-        return conditionTime
+
     
-    def gain_condition(self, informationTotal, para1, para2, para3, para4):
-        "根据用户输入信息获取筛选条件"
-        conditionRegular = informationTotal.isAvailable == 'Y'
+    def generate_filterboolcolumn(self):
+        """
+        根据用户输入信息获取筛选条件
+        To get filter condition:
+            1. we get condition name: para1, para2, para3, ...
+            2. we get each column of boolean data
+            3. we combine every condition boolean column
         
-        conditionTimeBegin = self.gain_conditionTime('beginTime', informationTotal)
-        conditionTimeEnd = self.gain_conditionTime('endTime', informationTotal)
+        """
+        para1 = self.parameter[0]
+        para2 = self.parameter[1]
+        para3 = self.parameter[2]
+        para4 = self.parameter[3]
+        
+        conditionRegular = self.informationTotal.isAvailable == 'Y'
+        
+        conditionTimeBegin = self.generate_timesegmentation('beginTime')
+        conditionTimeEnd = self.generate_timesegmentation('endTime')
         
         # 发车时间段筛选条件，根据用户的输入动态调整
         conditionBegin = pd.Series([False]*len(conditionTimeBegin))
@@ -211,40 +187,61 @@ leftTicketDTO.train_date={}&leftTicketDTO.from_station={}\
         conditionFastOrdinary = pd.Series([False]*len(conditionTimeEnd))
         for s in list(para3):
             sCat = ['Slow', 'Fast'][s == '1']
-            conditionFastOrdinary = conditionFastOrdinary | informationTotal['is' + sCat]
+            conditionFastOrdinary = conditionFastOrdinary | self.informationTotal['is' + sCat]
         # 车站筛选
         if para4[2] == 1:
-            conditionStation = pd.Series([True]*len(conditionTimeEnd))
+            conditionstation = pd.Series([True]*len(conditionTimeEnd))
         else:
-            conditionStation = informationTotal[para4[0]] == para4[1]
+            conditionstation = self.informationTotal[para4[0]] == para4[1]
         
         # 筛选条件列表
         conditionList = [conditionBegin, conditionEnd, conditionFastOrdinary, 
-                         conditionStation, conditionRegular]
+                         conditionstation, conditionRegular]
         condition = reduce(lambda x, y:x & y, conditionList)# 所有筛选条件取与
         
         return condition
 
-    def gotinfo(self, station):
-        url = self.gain_urlchecking(station, self.fromStationName(), self.toStationName(), self.traindate())
-        r = requests.get(url)
-        results = json.loads(r.content)
-        return self.gain_information(results)
-    
-    def printinfo(self):
-        print('\nInformation has been collected successfully!')
-        print("\n1. 出发时间\t\t2. 到达时间\t\t3. 高速/普通\n\
-4. 出发站\t\t5. 到达站\n\nFiltering...")
-    
-    def gotcondition(self, informationTotal, mapdata):
+    def generate_timesegmentation(self, beginEnd):
+        conditionTime1 = (self.informationTotal[beginEnd] >= '00:00') & \
+        (self.informationTotal[beginEnd] <= '06:00')# 筛选条件1：时间段00：00~06：00
+        conditionTime2 = (self.informationTotal[beginEnd] > '06:00') & \
+        (self.informationTotal[beginEnd] <= '09:00')# 和筛选条件1互斥
+        conditionTime3 = (self.informationTotal[beginEnd] > '09:00') & \
+        (self.informationTotal[beginEnd] <= '12:00')# 和筛选条件1互斥
+        conditionTime4 = (self.informationTotal[beginEnd] > '12:00') & \
+        (self.informationTotal[beginEnd] <= '15:00')# 和筛选条件1互斥
+        conditionTime5 = (self.informationTotal[beginEnd] > '15:00') & \
+        (self.informationTotal[beginEnd] <= '18:00')# 和筛选条件1互斥
+        conditionTime6 = (self.informationTotal[beginEnd] > '18:00') & \
+        (self.informationTotal[beginEnd] <= '21:00')# 和筛选条件1互斥
+        conditionTime7 = (self.informationTotal[beginEnd] > '21:00') & \
+        (self.informationTotal[beginEnd] <= '23:59')# 和筛选条件1互斥
         
-        para1 = self.gain_userInteraction(self.text1(), '1234567')
-        para3 = self.gain_userInteraction(self.text2(), '12')
+        columnTime = ['con1', 'con2', 'con3', 'con4', 'con5', 'con6', 'con7']
+        conditionTime = pd.DataFrame(list(zip(conditionTime1, 
+                                                   conditionTime2, 
+                                                   conditionTime3, 
+                                                   conditionTime4, 
+                                                   conditionTime5, 
+                                                   conditionTime6, 
+                                                   conditionTime7)))
+        conditionTime.columns = columnTime
         
-        beginList = self.gotmap('beginName', informationTotal)
-        endList = self.gotmap('endName', informationTotal)
+        return conditionTime
+    
+
+    
+    def generate_filteruserselect(self):
+        mapdata = self.results['data']['map']
+        
+        para1 = gain_userInteraction(text1(), '1234567')
+        para3 = gain_userInteraction(text2(), '12')
+        
+        beginList = self.gotmap('beginName')
+        endList = self.gotmap('endName')
         dfMap = pd.DataFrame({'出发':beginList})
         dfMap = dfMap.append(pd.DataFrame({'到达':endList}))
+        print(dfMap)
         
         print('出发'.center(13, '-'))
         for x in beginList:
@@ -265,27 +262,33 @@ leftTicketDTO.train_date={}&leftTicketDTO.from_station={}\
                 break
             except:
                 print('Error!! Please type again: ')
-        col = ['endName_map', 'beginName_map'][self.isbegin_end(dfMap, '出发', val)]
+        col = ['endName_map', 'beginName_map'][isbegin_end(dfMap, '出发', val)]# 有问题
         para4 = [col, val, flag]
         
+        parameter = [para1, '1234567', para3, para4]
         
-        return self.gain_condition(informationTotal, para1, 
-                                   '1234567', 
-                                   para3, 
-                                   para4)
+        return parameter
         
-    def gotfilter(self, condition, informationTotal):
+    def gotmap(self, cols):
+        "得到该次搜索出发/到达车站名字列表"
+        beginList = []
+        for x in self.informationTotal[cols]:
+            if (x not in beginList):
+                beginList = beginList + [x]
+        return beginList
+    
+    def gotfilter(self):
         columnNeeded = ['TrainNum', 'beginName_map', 'endName_map', 'beginTime', 
                     'endTime', 'diffTime', 'yw', 'yz', 'wz', 'erd', 'yd', 'sw']
         columnNeededMap = ['车次', '出发', '到达', '出发时', 
                         '到达时', '历时', '硬卧', '硬座', '无座', '二等', '一等', '商务']
         filteredData = pd.DataFrame()
-        filteredData[columnNeededMap] = informationTotal.loc[condition, columnNeeded]
+        filteredData[columnNeededMap] = self.informationTotal.loc[self.condition, columnNeeded]
         return filteredData
     
-    def printresult(self, filteredData, station):
+    def printresult(self, filteredData):
         
-        lStation = len(max([x for x in station.iloc[:,0]], key = len))
+        lstation = len(max([x for x in stationNameMapping.iloc[:,0]], key = len))
         
         if filteredData.empty:
             print('\nSorry~ There is no train left, try other filter combinations: ')
@@ -307,8 +310,8 @@ leftTicketDTO.train_date={}&leftTicketDTO.from_station={}\
                   + filteredData.columns[11].center(2,'-') + '|' )
             for i in range(len(filteredData)):
                 print(filteredData.iat[i, 0].ljust(5), end = '|')
-                print(filteredData.iat[i, 1].center(lStation), end = '|')
-                print(filteredData.iat[i, 2].center(lStation), end = '|')
+                print(filteredData.iat[i, 1].center(lstation), end = '|')
+                print(filteredData.iat[i, 2].center(lstation), end = '|')
                 print(filteredData.iat[i, 3].center(5), end = '|')
                 print(filteredData.iat[i, 4].center(5), end = '|')
                 print(filteredData.iat[i, 5].center(5), end = '|')
@@ -320,23 +323,27 @@ leftTicketDTO.train_date={}&leftTicketDTO.from_station={}\
                 print(filteredData.iat[i, 11].center(1), end = '|')
                 print()
                 
+                
+                
     def printcatalog(self):
         a = input("Press Enter to search new routine. \n(E)exit system\n")
         return [False, True]['e' in a.lower()]
 
+
 if __name__ == '__main__':
-    station = pd.read_csv('themappingfile.csv')
+
     while True:
-        ticket = ticketcheckingsystem('Clare')
+        ticket = ticketcheckingsystem()
         
         try:
-            mapdata, informationTotal = ticket.gotinfo(station)
             
             ticket.printinfo()
-        
-            filteredData = ticket.gotfilter(ticket.gotcondition(informationTotal, mapdata), informationTotal)
+            info = ticket.informationTotal
+            condi = ticket.condition
+            par = ticket.parameter
+            filteredData = ticket.gotfilter()
             
-            ticket.printresult(filteredData, station)
+            ticket.printresult(filteredData)
         except Exception as e:
             print(Exception,':',e)
             print('\nNetwork connection Error, Please check your network!')
